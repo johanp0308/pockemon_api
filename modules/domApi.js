94417f50df59015fea324eec463196aea9b84fe1
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-24 19:21:04 -0500

    feat: :sparkles: Refactoring the dom module
    Increased first-case functionality, in addition to adding cards to a container
    
    @johanp0308

--- modules/domApi.js
+++ modules/domApi.js
@@ -1,8 +1,16 @@
 
 
+const captfirts = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 const agregarTarjeta = (contain,data) =>{
-    
-    console.log(data)
+    contain.insertAdjacentHTML("beforeend",`
+    <div class="pokemon">
+        <img src="${data.img}" alt="">
+        <p id="${data.name}">${captfirts(data.name)}</p>
+    </div>
+    `);
 }
 
 
