6f97aee959735e950a096efc44fc6b4a687e647f
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-09-27 21:54:47 -0500

    feat: :beers: Added request methods
    1. Validation method if the pokemon exists in the API 2. The method to add the pokemon in the mockapi if it already exists changes it
    
    @johanp0308

--- modules/pokemonMet.js
+++ modules/pokemonMet.js
@@ -4,6 +4,50 @@ const peticiones = async (urlRes) => {
     return res;
 }
 
+const exitPoke = async(url,name) =>{
+    let res = await peticiones(url);
+    console.log(res);
+    let isExist = {};
+    res.forEach( ele => {
+        console.log(ele.name)
+        if(ele.name === name){
+            isExist = {
+                isExist:true,
+                id:ele.id
+            }
+        }else{
+            isExist = {
+                isExist:false,
+                id:0,
+            }
+        }
+    })
+    console.log(isExist);
+    return isExist
+}
+
+const enviarPokemon = async (url,data) =>{
+    let validation = await exitPoke(url,data.name) 
+    if(!(validation.isExist)){
+        let config = {
+            method:"POST",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(data)
+            }
+        let res = await (await fetch(url,config)).json();
+        console.log("Has enviado: ",res);
+    }else{
+        let config = {
+            method:"PUT",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify(data)
+        }
+        let res = await (await fetch(url+`/${validation.id}`,config)).json();
+        console.log("Has enviado: ",res);
+    }
+}
+
 export default{
-    peticiones
+    peticiones,
+    enviarPokemon
 }
